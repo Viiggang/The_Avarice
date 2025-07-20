dae83e6dc448807941d6d0f2f447e94be7314650
SadSmileXD <phycoeco@naver.com>
2025-07-20 20:43:34 +0900

    1차 업뎃

---
+++ DARK_Series-Colossal Boss/Assets/Boss/Boss-scripts/BossState/DeadState.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadState :IBossState
+{
+    public void Enter(BossController boss)
+    {
+
+    }
+    public void Execute(BossController boss)
+    {
+
+    }
+    public void Exit(BossController boss)
+    {
+
+    }
+}
