f43a77d00acbe3a3df1bf1e56e01ce5540108bbd
d <phycoeco@naver.com>
2025-09-22 13:26:20 +0900

    push

--- The_Avarice/Assets/MainUI/MainScene/ButtonEvents.cs
+++ The_Avarice/Assets/MainUI/MainScene/ButtonEvents.cs
@@ -1,49 +1,49 @@
-//using UnityEngine;
-//using UnityEngine.SceneManagement;
-//using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-//public class ButtonEvents : MonoBehaviour
-//{
-//    [SerializeField]private GameObject OptionUI;
-//    public void GameStart()
-//    {
-//        SceneManager.LoadScene("CharatorChoice"); // 로딩 안함
-//    }
-//    public void OptionActive()
-//    { 
-//        OptionUI?.SetActive(true);
-//    }
-//    public void OptionHide()
-//    {
-//        OptionUI?.SetActive(false);
-//    }
-//    public void Exit()
-//    {
-//#if UNITY_EDITOR
-//        UnityEditor.EditorApplication.isPlaying = false; // 에디터에서 실행 중지
-//#endif
+public class ButtonEvents : MonoBehaviour
+{
+    [SerializeField] private GameObject OptionUI;
+    public void GameStart()
+    {
+        SceneManager.LoadScene("CharatorChoice"); // 로딩 안함
+    }
+    public void OptionActive()
+    {
+        OptionUI?.SetActive(true);
+    }
+    public void OptionHide()
+    {
+        OptionUI?.SetActive(false);
+    }
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false; // 에디터에서 실행 중지
+#endif
 
-//        Application.Quit();
-//    }
+        Application.Quit();
+    }
 
-//    public void PaladinChoice()
-//    {
-//        PlayerMgr.instance.setPlayerType(Player_Type.Paladin);
-//        SceneLoader.Instance.ChangeScene("VillageScene");
-//    }
-//    public void IgnisChoice()
-//    {
-//        PlayerMgr.instance.setPlayerType(Player_Type.Ignis);
-//        SceneLoader.Instance.ChangeScene("VillageScene");
-//    }
-//    public void WindBreakerChoice()
-//    {
-//        PlayerMgr.instance.setPlayerType(Player_Type.WindBreaker);
-//        SceneLoader.Instance.ChangeScene("VillageScene");
-//    }
-//    public void SoulEaterChoice()
-//    {
-//        PlayerMgr.instance.setPlayerType(Player_Type.SoulEater);
-//        SceneLoader.Instance.ChangeScene("VillageScene");
-//    }
-//}
+    public void PaladinChoice()
+    {
+        PlayerMgr.instance.setPlayerType(Player_Type.Paladin);
+        SceneLoader.Instance.ChangeScene("VillageScene");
+    }
+    public void IgnisChoice()
+    {
+        PlayerMgr.instance.setPlayerType(Player_Type.Ignis);
+        SceneLoader.Instance.ChangeScene("VillageScene");
+    }
+    public void WindBreakerChoice()
+    {
+        PlayerMgr.instance.setPlayerType(Player_Type.WindBreaker);
+        SceneLoader.Instance.ChangeScene("VillageScene");
+    }
+    public void SoulEaterChoice()
+    {
+        PlayerMgr.instance.setPlayerType(Player_Type.SoulEater);
+        SceneLoader.Instance.ChangeScene("VillageScene");
+    }
+}
