33df1c12691a2374ead51dcca9b549bd6ac1df56
sadsmile <phycoeco@naver.com>
2026-01-19 14:42:21 +0900

    퀘스트 제작 중

--- The_Avarice/Assets/QuestAll/QuestActionHandler/QuestActionHandler.cs
+++ The_Avarice/Assets/QuestAll/QuestActionHandler/QuestActionHandler.cs
@@ -13,29 +13,80 @@ namespace QuestSystem
 
         void Update()
         {
+            if (Input.anyKeyDown == false || CurrentQuest == null)
+                return;
             InputAction();
         }
         public void InputAction()
         {
+            Debug.Log("½ÇÇà");
             if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
             {
                 if (Input.GetKeyDown(KeyCode.LeftShift))
                 {
-                    target = "ABC";
+                    target = "Dash";
                     execute();
-                    if (CurrentQuest.QuestState == QuestState.Complete)
-                    {
-                        CurrentQuest = null;
-                        Category = null;
-                        target = null;
-                    }
+                    IsCompleted();
+                }
+                else
+                {
+                    target = "move";
+                    execute();
+                    IsCompleted();
                 }
             }
+           
+            else if(Input.GetKeyDown(KeyCode.F))
+            {
+        
+                target = "inspection";
+                execute();
+                IsCompleted();
+            }
+            else if (Input.GetKeyDown(KeyCode.B))
+            {
+                //
+                target = "Inventory";
+                execute();
+                IsCompleted();
+            }
+            else if (Input.GetKeyDown(KeyCode.A))
+            {
+               
+                target = "Skill1";
+                execute();
+                IsCompleted();
+            }
+            else if (Input.GetKeyDown(KeyCode.S))
+            {
+             
+                target = "Skill2";
+                execute();
+                IsCompleted();
+            }
+            else if (Input.GetKeyDown(KeyCode.C))
+            {
+                
+                target = "Attack";
+                execute();
+                IsCompleted();
+            }
             //////////////////
-            
+
 
 
         }
+
+        private void IsCompleted()
+        {
+            if (CurrentQuest.QuestState == QuestState.Complete)
+            {
+                CurrentQuest = null;
+                Category = null;
+                target = null;
+            }
+        }
+
         public void SetQuest(Quest Data)
         {
             Category = Data.GetCategory();
