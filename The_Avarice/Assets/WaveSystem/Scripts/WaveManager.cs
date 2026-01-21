1a88794d1925c3f39d8be73a4c66a4e5ee661524
sadsmile <phycoeco@naver.com>
2026-01-21 14:05:22 +0900

    Wave 제작 중(스테이지 작업만 하면 됨)

--- The_Avarice/Assets/WaveSystem/Scripts/WaveManager.cs
+++ The_Avarice/Assets/WaveSystem/Scripts/WaveManager.cs
@@ -7,19 +7,32 @@ public class WaveManager : MonoBehaviour
 {
     [SerializeField]private WaveStage m_waveStage;
 
-    [SerializeField] GameObject[] WavePosition;
+    [SerializeField] GameObject[] WavePosition1;
+    [SerializeField] GameObject[] WavePosition2;
+    [SerializeField] GameObject[] WavePosition3;
 
     [SerializeField] private Dictionary<WaveStage, Action> sumon ;
-    
-
+    [SerializeField] private int m_livingCount = 0;
+    public int livingCount
+    {
+        get => m_livingCount;
+        set
+        {
+            m_livingCount = value;
+            if(m_livingCount ==0)
+            {
+                Up();
+            }
+        }
+    }
     private void Awake()
     {
         sumon = new Dictionary<WaveStage, Action>()
         {
             { WaveStage.Stage1,Wave1},
-         { WaveStage.Stage2,Wave1},
-          { WaveStage.Stage3,Wave1},
-           { WaveStage.End,Wave1},
+         { WaveStage.Stage2,Wave2},
+          { WaveStage.Stage3,Wave3},
+           { WaveStage.End,WaveEnd},
         };
     }
     public WaveStage waveStage
@@ -36,15 +49,44 @@ public class WaveManager : MonoBehaviour
 
     public void Wave1()
     {
-        WavePosition[2].SetActive(true);
-        WavePosition[3].SetActive(true);
-        WavePosition[6].SetActive(true);
-        WavePosition[7].SetActive(true);
+        foreach (var item in WavePosition1)
+        {
+            item.SetActive(true);
+        }
+    }
+    public void Wave2()
+    {
+        foreach (var item in WavePosition2)
+        {
+            item.SetActive(true);
+        }
+    }
+    public void Wave3()
+    {
+        foreach (var item in WavePosition3)
+        {
+            item.SetActive(true);
+        }
     }
 
     [ContextMenu("Start Wave")]
     public void Up()
     {
         waveStage+=1;
+    }
+
+    public void WaveEnd()
+    {
+
+    }
+
+    public   void countUp()
+    {
+        livingCount += 1;
+    }
+    public void CountDown()
+    {
+        livingCount -= 1;
+
     }
 }
